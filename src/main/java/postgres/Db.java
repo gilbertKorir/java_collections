0eef8f257772ab460579766ea17355dfc811ab4b
gilbertKorir <jerikorir@gmail.com>
2022-08-04 16:21:49 +0300

    ft:added postgresql database functions

--- src/main/java/postgres/Db.java
+++ src/main/java/postgres/Db.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 public class Db {
     public Connection connect(String dbname, String user, String pass){
         Connection connection = null;
-        Statement stmt = null;
+//        Statement stmt = null;
 
         try{
             Class.forName("org.postgresql.Driver");
@@ -58,12 +58,83 @@ public class Db {
             rs = statement.executeQuery(query);
             System.out.println(" Tha available data is as below:");
             while(rs.next()){
-                System.out.println(rs.getString("empid"));
-                System.out.println(rs.getString("name"));
-                System.out.println(rs.getString("address"));
+                System.out.println(rs.getString("empid") + " "+ rs.getString("name") + " " + rs.getString("address"));
+//                System.out.println(rs.getString("name"));
+//                System.out.println(rs.getString("address"));
             }
         } catch(Exception e){
             System.out.println(e);
         }
     }
+    //updating the database
+    public void updateDb(Connection connection, String tbname, String oldName, String newName){
+        Statement statement;
+        try{
+            String query = String.format("update %s set name = '%s' where name = '%s'", tbname, newName, oldName);
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Data updated successfully");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+    //Search by name
+    public void searchByName(Connection connection, String tbname, String name){
+        Statement statement;
+        ResultSet rs = null;
+        try {
+            String query = String.format("select * from %s where name = '%s'", tbname, name);
+            statement = connection.createStatement();
+            rs = statement.executeQuery(query);
+            while(rs.next()){
+                System.out.println(rs.getString("empid") + " "+ rs.getString("name") + " " + rs.getString("address"));
+            }
+            System.out.println("Above name was found");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    //Search by name
+    public void searchById(Connection connection, String tbname, int id){
+        Statement statement;
+        ResultSet rs = null;
+        try {
+            String query = String.format("select * from %s where empid = %s", tbname, id);
+            statement = connection.createStatement();
+            rs = statement.executeQuery(query);
+            while(rs.next()){
+                System.out.println(rs.getString("empid") + " "+ rs.getString("name") + " " + rs.getString("address"));
+            }
+            System.out.println("Above ID was found");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    //dELETE ROW
+    public void deleteRowByName(Connection connection, String tbname, String name){
+        Statement statement;
+        try{
+            String query = String.format("delete from %s where name = '%s'", tbname, name);
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Row deleted successfully");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    //delete table
+    public void deleteTable(Connection connection, String tbname){
+        Statement statement;
+        try{
+            String query = String.format("drop table %s",tbname);
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Table deleted you can create a new one using the function create()");
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
 }
--- src/main/java/postgres/Postgre.java
+++ src/main/java/postgres/Postgre.java
@@ -7,7 +7,12 @@ public class Postgre {
         Db db = new Db();
        Connection connection = db.connect("testdb","postgres","1234");
 //       db.create(connection, "employee");
-//        db.insertTable(connection, "employee", "John", "Kenya");
-        db.readData(connection, "employee");
+//        db.insertTable(connection, "employee", "Fred", "Canada");
+//        db.updateDb(connection, "employee", "John", "Pauline");
+//        db.searchByName(connection, "employee", "Fred");
+//        db.searchById(connection, "employee", 2);
+//        db.deleteRowByName(connection, "employee", "Fred");
+//        db.readData(connection, "employee");
+        db.deleteTable(connection,"employee");
     }
 }
